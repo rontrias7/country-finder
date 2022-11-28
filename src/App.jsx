58967c6867bebe0604c10087e04b9bf8a2b713cb
rontrias7 <rontrias7@gmail.com>
2022-11-28 19:03:03 -0300

    init

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import './app.css';
+import DisplayResults from './components/DisplayResults';
+
+
+
+
+
+
+function App() {
+
+  return (
+
+    <div className="global-container">
+    <h2 className="title">C🌎UNTRY FINDER</h2>
+    <DisplayResults />
+    </div>
+  );
+
+}
+
+
+export default App;
---
+++ src/components/DisplayResults.jsx
@@ -0,0 +1,140 @@
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+import {motion, AnimatePresence} from 'framer-motion'
+
+
+
+
+
+function DisplayResults() {
+
+  const [countries, setCountries] = useState([]);
+  const [searchedCountry, setsearchedCountry] = useState('');
+
+
+    useEffect(() => {
+
+      axios
+      .get("https://restcountries.com/v3.1/all")
+      .then(response =>  {
+        console.log("fetched");
+      setCountries(response.data);
+
+      })
+      },
+      []);
+
+      function toggleSearch(e) {
+        setsearchedCountry(e.target.value)
+
+      }
+
+
+
+
+      let dataFilter = countries.filter( country => country.name["common"].toLowerCase().startsWith(searchedCountry.toLowerCase()));
+      console.log(dataFilter);
+
+
+  return (
+    <div className="results" >
+        <input className="input-search" value={searchedCountry} onChange={toggleSearch} />
+        <div className="results-list">
+        <ul>
+
+        {
+
+        /*
+        * BEGINNING of the chain of conditional rendering.
+        * I chose to use conditional statements instead of switch because is cleaner and visually helps
+        * to make changes on the go.
+        *
+        * */
+
+        searchedCountry.length === 0 && countries.length > 100  ?
+          <p className='notification'>Please type a country in the input field.</p>
+
+          :
+
+          /* if data filtered is just one, then display a more detailed panel of information */
+          dataFilter.length === 1 ?
+                dataFilter.map( country => {
+
+                  return (
+                  <motion.li animate={{ opacity: [0, 0.3, 0.5, 0.8, 1] }}   key={country.ccn3}>
+                    <h1>{country.name.common} <span> {country.flag}</span></h1>
+                    <p><span>{country.name.official}</span><span> | Population: {country.population}</span></p>
+                    <img src={country.flags.png} alt={country.name.official+" flag"} />
+                  </motion.li>)
+                })
+                    /* else if the are not countries that match and something was typed in the input*/
+                      :
+                      searchedCountry.length > 0 && dataFilter.length === 0 ?
+                        <p className="notification" >The country you're looking for doesn't exist in our database.</p>
+
+
+                               :
+
+                               // Slicing to display just 5 items of the full list of matches
+                               dataFilter.slice(0,5).map( (country, index) => {
+
+                                    console.log(dataFilter.slice(0,5).length)
+                                    return  (
+                                      <AnimatePresence>
+                                        <motion.li
+                                        transition={{duration : 0.2}}
+                                        animate={{ opacity: [0, 0.5, 0.3, 1, 0.5, 1] }}
+                                        exit={{opacity : [1, 0.8, 0.5, 0.3, 0]}}
+                                        style={
+                                          // filtering the last item if the remainder notification isn't displayed
+                                          // to correct some rect angles in the element.
+                                          index === dataFilter.slice(0,5).length-1 && dataFilter.length - 5 < 5  ?
+                                        {borderRadius: "16px 16px 0px 0px;"}
+                                        :
+
+                                        (index % 2) ?
+                                        {backgroundColor: "#fafafa"}
+                                        :
+                                        {backgroundColor: "white"}}
+                                        key={country.ccn3}>
+                                          <span
+                                          className='matching-text'>
+                                          {country.name.common.slice(0, searchedCountry.length)}
+                                          </span>
+                                          <span
+                                          style={{color: "#8badf1"}}>
+                                            {country.name.common.slice(searchedCountry.length)}
+                                          </span>
+                                          <span className='official-name'>  ({country.name.official})</span>
+
+
+
+
+                                        </motion.li>
+                                      </AnimatePresence>
+                                      )
+
+                                }
+
+
+
+                               )}
+
+                              {
+
+                              // If we've various countries, then, display just five and show the reminder.
+                              searchedCountry.length > 0 && dataFilter.length > 1 && (dataFilter.length - 5) > 0 ?
+                                <p className='remainder-notification'>and {dataFilter.length - 5} more.</p>
+                               :
+                               undefined
+                               }
+        </ul>
+        </div>
+    </div>
+
+        )
+
+
+}
+
+export default DisplayResults;
