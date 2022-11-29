9b5ed1fd190f079cf58b84db2c6a5525653fc170
rontrias7 <rontrias7@gmail.com>
2022-11-29 18:56:58 -0300

    Minor UI changes.

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,8 @@ function App() {
   return (
 
     <div className="global-container">
-    <h2 className="title">C🌎UNTRY FINDER</h2>
+    <h1 className="title">C🌎UNTRY FINDER</h1>
+    <p className='ron-footer'>Powered by  <a href="https://www.reactjs.org" target="_blank" rel="noopener noreferrer">React</a>, <a href="https://www.vitejs.dev" target="_blank" rel="noopener noreferrer"> Vite </a>  &  <a href="https://www.restcountries.com" target="_blank" rel="noopener noreferrer"> REST Countries</a>. | <a href="mailto:rontrias7@gmail.com" target="_blank" rel="noopener noreferrer">Ron Trias</a> </p>
     <DisplayResults />
     </div>
   );
